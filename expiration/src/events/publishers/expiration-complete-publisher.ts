e4aca79f705037b78828994a0dbd39866a0d104f
federicoviscomi <federicoviscomi@gmail.com>
2021-02-13 21:55:28 +0000

    remove stephen

--- expiration/src/events/publishers/expiration-complete-publisher.ts
+++ expiration/src/events/publishers/expiration-complete-publisher.ts
@@ -2,7 +2,7 @@ import {
   Subjects,
   Publisher,
   ExpirationCompleteEvent,
-} from '@sgtickets/common';
+} from '@procigatto/common';
 
 export class ExpirationCompletePublisher extends Publisher<
   ExpirationCompleteEvent
--- payments/src/events/listeners/order-created-listener.ts
+++ payments/src/events/listeners/order-created-listener.ts
@@ -1,5 +1,5 @@
 import { Message } from 'node-nats-streaming';
-import { Listener, OrderCreatedEvent, Subjects } from '@sgtickets/common';
+import { Listener, OrderCreatedEvent, Subjects } from '@procigatto/common';
 import { queueGroupName } from './queue-group-name';
 import { Order } from '../../models/order';
 
